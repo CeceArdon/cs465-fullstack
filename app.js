61149c5f3030fc8d6e5756d92a92a8ca9b161128
Concepcion Ardon <concepcion.ardonarias@snhu.edu>
2023-03-19 10:56:22 -0500

    Convert travel template from static data to dynamic JSON

---
+++ app.js
@@ -0,0 +1,56 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const hbs = require('hbs');
+
+const indexRouter = require('./app_server/routes/index');
+const usersRouter = require('./app_server/routes/users');
+const travelRouter = require('./app_server/routes/travel');
+
+const app = express();
+app.get('/', indexRouter);
+app.get('/contact', (req, res) => res.render('contact', {contactSelected: req.path == '/contact'}));
+app.get('/rooms', (req, res) => res.render('rooms', {roomsSelected: req.path == '/rooms'}));
+app.get('/meals', (req, res) => res.render('meals', {mealsSelected: req.path == '/meals'}));
+app.get('/news', (req, res) => res.render('news', {newsSelected: req.path == '/news'}));
+app.get('/about', (req, res) => res.render('about', {aboutSelected: req.path == '/about'}));
+
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'app_server', 'views'));
+
+// register handlebars partials (https://www.npmjs.com/package/hbs)
+hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
+
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/travel', travelRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
