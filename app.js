5cb17dfc19ffda648802c0f27c4296062821d574
Concepcion Ardon <concepcion.ardonarias@snhu.edu>
2023-03-26 15:25:05 -0500

    Create object schema with Mongoose; populate MongoDB

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+
+require('./app_server/models/db');
+
 const hbs = require('hbs');
+
+
 
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
---
+++ app_server/models/db.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const host = process.env.DB_HOST || '127.0.0.1';
+const conn_uri = `mongodb://${host}/travlr`;
+
+const {seed} = require('./seed');
+
+require('./trips'); // Register models
+
+mongoose.connection.on('connected', () => console.log('CONNECTED!'));
+mongoose.connection.on('error', err => console.log(err));
+mongoose.connection.on('disconnected', () => console.log('DISCONNECTED!'));
+
+mongoose.set('strictQuery', false);
+
+//Kill MongoDB connections before existing app
+const gracefulShutdown = (msg, callback) => {
+  mongoose.connection.close( () => {
+    console.log(`Mongoose disconnected due to ${msg}`);
+    callback();
+  });
+}
+
+process.once('SIGUSR2', () => 
+  gracefulShutdown('nodemon restart', () => process.kill(process.pid, 'SIGUSR2')));
+process.on('SIGINT', () => 
+  gracefulShutdown('app termination', () => process.exit(0)));
+
+// ....MongoDB graceful shutdown code omitted for brevity
+
+async function main() {
+    await mongoose.connect(conn_uri);
+    await seed(); 
+}
+
+main().catch(console.log);
