83b71fd8d3e42bf160541afe9cafac60ae848245
Concepcion Ardon <concepcion.ardonarias@snhu.edu>
2023-04-02 21:43:54 -0500

    Create new application API

--- app.js
+++ app.js
@@ -4,15 +4,14 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-require('./app_server/models/db');
-
 const hbs = require('hbs');
-
+require('./app_api/models/db');
 
 
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
 const travelRouter = require('./app_server/routes/travel');
+const apiRouter = require('./app_api/routes/index');
 
 const app = express();
 app.get('/', indexRouter);
@@ -41,6 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
